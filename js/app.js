909cdb1cb4b813564275b35e88cf7b66cb0dc66d
michael williams <manicdesigner@gmail.com>
2018-09-20 13:57:42 -0400

    refactor to box-container from box-restraint for better variable naming.

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@
  * License: MIT, You may use this code in full or in part With written acknowledgement
  * Inside the code or in the README.md referencing Michael Williams.
  */
+
+// bubble event sent from buttons up to box-container where i will bubble delegate.
+const boxContainer = document.querySelector('.box-container');
+
+
+/**
+ *
+ * @type {buttons}
+ */
+
