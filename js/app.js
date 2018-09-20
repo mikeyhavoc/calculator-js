4a51150905c809f86a10d5a163816abf18ea4b15
michael williams <manicdesigner@gmail.com>
2018-09-20 10:45:45 -0400

    doc with license agreement.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Project: Calculator-Js
+ * Author: Michael Williams
+ * Version: 2.0.0
+ * date: 2018-09-20 10:41:25 -0500
+ * License: MIT, You may use this code in full or in part With written acknowledgement
+ * Inside the code or in the README.md referencing Michael Williams.
+ */
