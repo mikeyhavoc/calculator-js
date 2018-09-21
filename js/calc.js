dc489ba55a4a2d0e96f1d2383f917728de550e8a
michael williams <manicdesigner@gmail.com>
2018-09-21 13:41:29 -0400

    prev....operator too null. add handleSymbol with switch statement.

--- js/calc.js
+++ js/calc.js
@@ -8,11 +8,12 @@
 
 let runningTotal = 0;
 let buffer = '0';
-let previousOperator;
+let previousOperator = null;
 const screen = document.querySelector('.screen');
 
 document.querySelector('.calc-buttons').addEventListener('click', function(event) {
-   buttonClick(event.target.value);
+ console.log(event.target.value);
+   buttonClick(event.target.innerText);
 });
 
 /**
@@ -22,8 +23,10 @@ document.querySelector('.calc-buttons').addEventListener('click', function(event
  */
 function buttonClick(value) {
     if(isNaN(parseInt(value))) {
+
         handleSymbol(value);
     } else {
+
             handleNumber(value);
     }
 
@@ -43,12 +46,32 @@ function handleNumber(value) {
     rerender();
 }
 
-function handleSymbol(value) {}
 
-/**
- * rerender
- * pushes buffer to screen.
- */
-function rerender() {
-    screen.innerText = buffer;
+function handleSymbol(value) {
+    const arrow = `&larr;`;
+    switch(value) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = ' ' + runningTotal;
+            runningTotal = 0;
+            break;
+        case arrow:
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        default:
+            handleMath(value);
+            break;
+    }
 }
